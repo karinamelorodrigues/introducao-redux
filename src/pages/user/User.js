cf06af213de67c2899e4e9644e9b26666b000ef1
Karina Melo Rodrigues <karina.rodrigues@nb-will0576.avista.local>
2022-01-30 22:14:37 -0300

    feat: adding components and pages

---
+++ src/pages/user/User.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import UserForm from './UserForm';
+
+class User extends Component {
+  render() {
+    let list = [
+        {
+            name:"Karina",
+            email:"karinamelorodrigues@@gmail.com"
+        },
+        {
+            name:"João",
+            email:"joao@gmail.com"
+        }
+    ]
+
+    return (
+        <div>
+            <UserForm></UserForm>
+            <table border="1">
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+                {list.map((item) => {
+                    return <tr>
+                        <td>
+                            {item.name}
+                        </td>
+                        <td>
+                            {item.email}
+                        </td>
+                    </tr>
+                })}
+            </table>
+        </div>
+    );
+  }
+}
+
+export default User;
